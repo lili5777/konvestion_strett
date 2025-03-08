afc7db4b523263f93bc32a3df62cf79638470061
kelompok1 <hasniawati979@gmail.com>
2025-03-08 23:02:46 +0800

    rggvfhfh

---
+++ script.js
@@ -0,0 +1,17 @@
+function konver() {
+  let input = document.getElementById("nilai").value;
+  let jarak1 = document.getElementById("jarak1").value;
+  let jarak2 = document.getElementById("jarak2").value;
+  let output = document.getElementById("output");
+
+  if (jarak1 == "meter" && jarak2 == "kilometer") {
+    const hasil = input / 1000;
+    output.innerText = "Nilai Input: " + hasil;
+  } else if (jarak1 == "kilometer" && jarak2 == "centimeter") {
+    const hasil = input * 100000;
+    output.innerText = "Nilai Input: " + hasil;
+  } else if (jarak1 == "kilometer" && jarak2 == "meter") {
+    const hasil = input * 1000;
+    output.innerText = "Nilai Input: " + hasil;
+  }
+}
