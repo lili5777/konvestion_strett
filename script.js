329c3de2c2ac2eff7931a31b38b4f79e3cf7641e
pipiw20 <safira.syah86@gmail.com>
2025-03-08 22:50:29 +0800

    hjjhjh

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-let input=document.getElementById('nilai').value;
+let input = document.getElementById("nilai").value;
+{
+  if (jarak1 === "centi meter") {
+    if (jarak2 === "kilo meter") {
+      nilai / 100000;
+    } else if (jarak2 === "meter") {
+      nilai / 100;
+    }
+  }
+}
