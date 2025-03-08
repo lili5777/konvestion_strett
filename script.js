7f131d2d49f6c1ff9067916b89527f0ea81d9fdb
lili5777 <zailyanzali@gmail.com>
2025-03-08 22:45:22 +0800

    update2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-let input=document.getElementById('nilai').value;
+function konver() {
+  let input = document.getElementById("nilai").value;
+  let jarak1=document.getElementById("jarak1").value;
+  let jarak2 = document.getElementById("jarak2").value;
+  let output = document.getElementById("output");
+
+  if(jarak1=='meter' && jarak2=='kilometer'){
+    const hasil= input/1000;
+    output.innerText = "Nilai Input: " + hasil;
+  } 
+  
+}
